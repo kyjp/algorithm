b5769a871f2ad9fdd35cc26682683c6dc85cd23f
kyjp <numunumu17@hotmail.co.jp>
2024-10-22 23:16:37 +0900

    dp

--- SubsetSum.js
+++ SubsetSum.js
@@ -6,28 +6,44 @@ const input = [
 const [N, S] = input.shift().split(" ").map(i => Number(i))
 const A = input.shift().split(" ").map(i => Number(i))
 
-const dp = []
+A.unshift(0)
 
-for(let i = 0; i < N - 1; i++) {
-    for(let j = i + 1; j < N; j++) {
-        dp.push(A[i] + A[j])
+const dp = [...Array(N + 1)].map(() => Array(S + 1).fill("×"))
+
+
+dp[0][0] = "○"
+for(let i = 1; i <= N; i++) {
+    for(let j = 0; j <= S; j++) {
+        if(dp[i - 1][j] === "○") {
+            dp[i][j] = "○"
+            continue
+        }
+        if(dp[i - 1][j - A[i]] === "○") {
+            dp[i][j] = "○"
+            continue
+        }
     }
 }
 
-let flg = false
+
 console.log(dp)
 
-for(let i = 0; i < N; i++) {
-    if(dp[i] + A[i] === S) {
-        flg = true
-        break
-    }
-}
-
-if(flg) {
+if(dp[N][S] === "○") {
     console.log("Yes")
+
+    // 復元
+    let N2 = N
+    let S2 = S
+    const result = []
+    while(true) {
+        if(N2 === 0 || S2 < 0) break
+        if(A[N2] <= S2 && dp[N2 - 1][S2 - A[N2]] === "○" && dp[N2][S2] === "○") {
+            result.push(A[N2])
+            S2 -= A[N2]
+        }
+        N2--
+    }
+    console.log(result)
 }else{
     console.log("No")
 }
-
-let total = N
