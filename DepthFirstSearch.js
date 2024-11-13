aefdb20dbc33fc5e8e54042f58753888438f1de1
kyjp <numunumu17@hotmail.co.jp>
2024-11-13 21:36:19 +0900

    update

--- DepthFirstSearch.js
+++ DepthFirstSearch.js
@@ -7,3 +7,29 @@ const input = [
     '4 5',
     '4 6'
 ]
+
+const dfs = (v, adj, visited) => {
+    visited[v] = true
+    for(let neighbor of adj[v]) {
+        if(!visited[neighbor]) {
+            dfs(neighbor, adj, visited)
+        }
+    }
+}
+
+const [N, M] = input.shift().split(' ').map(i => Number(i))
+const adj = Array.from({length: N + 1}, () => [])
+for(let i = 0; i < M; i++) {
+    const [A, B] = input.shift().split(' ').map(i => Number(i))
+    adj[A].push(B)
+    adj[B].push(A)
+}
+
+const visited = new Array(N + 1).fill(false)
+dfs(1, adj, visited)
+
+if(visited.slice(1).every(v => v)) {
+    console.log("The graph is connected");
+} else {
+    console.log("The graph is not connected");
+}
